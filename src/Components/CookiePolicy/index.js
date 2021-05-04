4e43e49622f022193919eca88aa72ea5fcc29e84
davis nicholas <davisnnicholas235@gmail.com>
2021-05-04 16:03:54 +0530

    privacy

--- src/Components/CookiePolicy/index.js
+++ src/Components/CookiePolicy/index.js
@@ -18,8 +18,7 @@ const CookiePolicy = () => {
         <div style={{display: 'flex',justifyContent: 'center'}}>
             <div style={{width: '70%',display: 'flex',alignItems: 'flex-start',flexDirection: 'column'}}>
                 <h1 style={{fontWeight:600,color: '#1CAF95'}}>{heading}</h1>
-                <div style={{textAlign: 'left',color:"grey",fontSize:17,fontWeight:500,lineHeight:1.5}}>
-                {content}
+                <div style={{textAlign: 'left',color:"grey",fontSize:17,fontWeight:500,lineHeight:1.5,whiteSpace:"pre-wrap"}} dangerouslySetInnerHTML={{ __html: content }}>
                 </div>
                 
             </div>
