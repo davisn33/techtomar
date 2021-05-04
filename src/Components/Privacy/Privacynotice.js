8666a58c3a3f1c3089883cc1a9b17eed18e47878
davis nicholas <davisnnicholas235@gmail.com>
2021-05-04 15:39:42 +0530

    privacy

--- src/Components/Privacy/Privacynotice.js
+++ src/Components/Privacy/Privacynotice.js
@@ -19,7 +19,7 @@ const Privacynotice = () => {
         <div style={{display: 'flex',justifyContent: 'center'}}>
             <div style={{width: '70%',display: 'flex',alignItems: 'flex-start',flexDirection: 'column'}}>
                 <h1 style={{fontWeight:600,color: '#1CAF95'}}>{heading}</h1>
-                <div style={{textAlign: 'left',color:"grey",fontSize:17,fontWeight:500,lineHeight:1.5}}>
+                <div style={{textAlign: 'left',color:"grey",fontSize:17,fontWeight:500,lineHeight:1.5,whiteSpace:"pre-wrap"}}>
                 {content}
                 </div>
                 
